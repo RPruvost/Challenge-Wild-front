a37ff32de7ce3e6ece41bb5eb198acc28ff4eeeb
Roméo Pruvost <romeo.pruvost3007@gmail.com>
2022-02-13 22:39:03 +0100

    front created

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+
+const Footer = () => {
+    return ( 
+        
+    <footer className= {styles.footer}>
+        <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
+      </footer>
+     );
+}
+ 
+export default Footer;
